dea19e18b5d7c1b295d92decdc3fc95a75bd9dcd
yangbeom <yangbeomsu@gmail.com>
2017-07-13 16:46:47 +0900

    init v1.3.0

--- app/main.js
+++ app/main.js
@@ -2,12 +2,10 @@ const electron = require('electron')
 const path = require('path')
 const url = require('url')
 const fs = require('fs')
-const PlayerControl = require('./playercontrol.js')
 const scripts = require('./scripts.js')
 
-const {app, ipcMain, BrowserWindow, session} = electron
+const {app, ipcMain, BrowserWindow, session, globalShortcut} = electron
 
-let enableMPRIS = false;
 
 let configs
 let pluginName
@@ -25,28 +23,10 @@ catch(e)
     configs = {"volume": "50", "playerSkin": "white"}
 }
 
-switch (process.platform){
-    case 'darwin':
-        pluginName = 'pepperflash/PepperFlashPlayer.plugin'
-        break
-    case 'linux':
-        pluginName = 'pepperflash/libpepflashplayer.so'
-        enableMPRIS = true
-        break
-    default:
-        //Maybe MS Windows?
-        console.error('Cannot load pepper flash for this platform.');
-}
-
-const playerControl = new PlayerControl(enableMPRIS);
-
-app.commandLine.appendSwitch('ppapi-flash-path', 
-                             path.join(__dirname, pluginName))
-
 
 function createWindow(){
     var preference = {
-        width: 384, 
+        width: 384,
         height: 710,
         resizable: false, //크기 변환 불가능
         webPreferences:{
@@ -56,7 +36,7 @@ function createWindow(){
     }
 
     mainWindow = new BrowserWindow(preference)
-    mainWindow.loadURL('http://music.bugs.co.kr/newPlayer?autoplay=false')
+    mainWindow.loadURL('http://music.bugs.co.kr/newPlayer?autoplay=false&html5=true')
 
     mainWindow.show()
 }
@@ -64,51 +44,44 @@ function createWindow(){
 app.on('ready', () =>{
     console.log(configs)
     createWindow()
+        try
+    {
+        configs = require('./configs.json')
+    }
+    catch(e)
+    {
+        configs = {"volume": "50", "playerSkin": "white"}
+    }
+    console.log(configs)
+    createWindow()
+    console.log(process.versions.electron)
 
-    // Initialize player control.
-    playerControl.init();
+    //shrotcut 전역 등록
+    globalShortcut.register('MediaPlayPause',() =>{
+        console.log('Press MediaPlayPause')
+        mainWindow.webContents.executeJavaScript(scripts.MPP)
+    })
 
-    playerControl.on('play', () => {
-        mainWindow.webContents.executeJavaScript(scripts.MP);
-    });
 
-    playerControl.on('pause', () => {
-        mainWindow.webContents.executeJavaScript(scripts.MS);
-    });
+    globalShortcut.register('MediaNextTrack', () =>{
+        mainWindow.webContents.executeJavaScript(scripts.MNT)
+    })
 
-    playerControl.on('stop', () => {
-        //TODO: Hack control to emulate 'real stop'
-        mainWindow.webContents.executeJavaScript(scripts.MS);
-    });
-
-    playerControl.on('playpause', () => {
-        mainWindow.webContents.executeJavaScript(scripts.MPP);
-    });
-
-    playerControl.on('next', () => {
-        mainWindow.webContents.executeJavaScript(scripts.MNT);
-    });
-
-    playerControl.on('previous', () => {
-        mainWindow.webContents.executeJavaScript(scripts.MPT);
-    });
-
-    //IPC listener for metadata
-    ipcMain.on('metadata', (evt, data) => {
-        playerControl.updateMetadata(data);
-    });
+    globalShortcut.register('MediaPreviousTrack', () =>{
+        mainWindow.webContents.executeJavaScript(scripts.MPT)
+    })
 
     //마지막 스킨설정 세팅
-    session.defaultSession.cookies.set({url:'http://music.bugs.co.kr/', 
+    session.defaultSession.cookies.set({url:'http://music.bugs.co.kr/',
                                         name:'playerSkin',
                                         value: configs.playerSkin},
-                                        (error) => {}) 
+                                        (error) => {})
     //마지막 볼륨 세팅
     session.defaultSession.cookies.set({url:'http://music.bugs.co.kr/',
                                         name: 'volume',
                                         value: configs.volume},
                                         (error) => {})
-//쿠키값 변경시 설정변경  
+//쿠키값 변경시 설정변경
     session.defaultSession.cookies.on('changed',
         (event, cookie, cause, removed) => {
             if(cookie.domain == 'music.bugs.co.kr')
@@ -146,7 +119,7 @@ app.on('browser-window-created', (e, window) =>{
             if(title ==='로그인')
             {
                 nwindow.on('page-title-updated',(e, title) => {
-                    if(title === '벅스 음악 플레이어!')
+                    if(title === '벅스')
                     {
                         mainWindow.reload()
                     }
@@ -158,10 +131,9 @@ app.on('browser-window-created', (e, window) =>{
 
 //shortcut 등록 해제, settings값 저장
 app.once('will-quit', () =>{
-    fs.writeFile(path.join(__dirname,'configs.json'), 
-                 JSON.stringify(configs), 
+    fs.writeFile(path.join(__dirname,'configs.json'),
+                 JSON.stringify(configs),
                  (error) => {})
 
-    playerControl.cleanup();
 })
 
